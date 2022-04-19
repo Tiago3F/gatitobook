f195e18906f989b1690826c0622cf3a5f3d05165
tiago <tiago.moreira@timebrisa.com.br>
2022-04-19 10:23:48 -0300

    feat: :sparkles: Login e Cadastro de usuários

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,25 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
+import { MensagemModule } from '../componentes/mensagem/mensagem.module';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    NovoUsuarioComponent,
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule,
+    MensagemModule
   ],
   exports: [HomeComponent]
 })
